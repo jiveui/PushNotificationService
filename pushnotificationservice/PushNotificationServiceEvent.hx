d6b0b94be6508700835bce297c86f3eebe192725
maximshteert <ma3ks.smv@gmail.com>
2016-10-06 16:13:56 +0700

    fixed var name

--- pushnotificationservice/PushNotificationServiceEvent.hx
+++ pushnotificationservice/PushNotificationServiceEvent.hx
@@ -8,23 +8,23 @@ class PushNotificationServiceEvent extends Event {
     public static inline var MESSAGE_RECEIVED = "message_received";
 
     public var token(default, null) : String = null;
-    public var receivedMeassage(default, null) : String = null;
+    public var receivedMessage(default, null) : String = null;
 
     public function new(type: String, token: String, receivedMessage: String = "") {
         super(type, true, true);
 
         this.token = token;
-        this.receivedMeassage = receivedMeassage;
+        this.receivedMessage = receivedMessage;
     }
 
     public override function clone() : Event {
-        return new PushNotificationServiceEvent(type, token, receivedMeassage);
+        return new PushNotificationServiceEvent(type, token, receivedMessage);
     }
 
     public override function toString() : String {
         var s = "[PushNotificationServiceEvent type=" + type;
         s += " token=" + token;
-        s += "receivedMeassage" + receivedMeassage;
+        s += "receivedMeassage" + receivedMessage;
         s += "]";
         return s;
     }
